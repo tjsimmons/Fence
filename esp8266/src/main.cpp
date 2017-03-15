e87e8da7e2873f5e147949f07f7f5dad6b96031b
T.J. Simmons <tj@tjsimmons.net>
2017-03-14 20:08:23 -0500

    there, some details in my source

--- esp8266/src/main.cpp
+++ esp8266/src/main.cpp
@@ -1,6 +1,18 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 
+/*
+TODO:
+  On device initialization, run as a AP with web server
+  Connect to device from a computer, and from a dropdown
+  choose the SSID and enter the password of network ESP82666
+  should connect to.
+
+  Once configuration is complete, should be written to the device
+  so that on subsequent boots, the device starts up in client mode
+  and calls home to our web service for commands.
+*/
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 
